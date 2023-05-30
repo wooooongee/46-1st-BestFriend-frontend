e4de5c6a117c5a4ce9c0052ae1efaa3228ca428e
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-30 18:40:33 +0900

    login layout 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main className="login-container">
+      <img
+        className="gron-logo"
+        src="/images/Login/gron-logo.png"
+        alt="gronLogo"
+      />
+      <input className="user-id-input" placeholder="이메일" />
+      <input className="user-password-input" placeholder="비밀번호" />
+      <button className="login-btn">로그인</button>
+      <button className="signup-btn">계정 만들기</button>
+    </main>
+  );
 };
 
 export default Login;
