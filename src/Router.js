2333864703dc550ed9636746353c6669f36f7e44
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-30 15:54:39 +0900

    초기세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Main } from './pages/Main/Main';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      {/* <Nav /> */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Main.scss';
+
+export const Main = () => {
+  return <div>asdf</div>;
+};
