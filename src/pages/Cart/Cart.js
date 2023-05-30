1c6208b99f37e3cf44010df6f6fa7f7f1a29c2b6
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-30 22:27:35 +0900

    Add : Cart UI 추가

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import './Cart.scss';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="cart">
+      <h1 className="title">장바구니</h1>
+      <div className="cart-container">
+        <main className="cart-main">
+          <div className="cart-box">
+            <div className="cart-img">
+              <img
+                src="/images/ProductDetail/plant-21.jpg"
+                alt="product-img"
+                className="img"
+              />
+            </div>
+            <div className="cart-content">
+              <div className="content">
+                <h1 className="title">몬스테라</h1>
+                <p>수량 : 2개</p>
+                <button className="btn">삭제</button>
+              </div>
+              <div className="total">
+                <p>총 가격:20,000원</p>
+              </div>
+            </div>
+          </div>
+          <div className="cart-box">
+            <div className="cart-img">
+              <img
+                src="/images/ProductDetail/plant-21.jpg"
+                alt="product-img"
+                className="img"
+              />
+            </div>
+            <div className="cart-content">
+              <div className="content">
+                <h1 className="title">몬스테라</h1>
+                <p>수량 : 2개</p>
+                <button className="btn">삭제</button>
+              </div>
+              <div className="total">
+                <p>총 가격:20,000원</p>
+              </div>
+            </div>
+          </div>
+        </main>
+        <aside className="cart-aside">
+          <h1 className="title">주문금액</h1>
+          <div className="price">
+            <div className="price-sort">
+              <p>상품금액</p>
+              <p>40,000원</p>
+            </div>
+            <div className="price-sort">
+              <p>배송비</p>
+              <p>무료</p>
+            </div>
+          </div>
+          <div className="total-price">
+            <div className="price-sort">
+              <p>총 결제금액</p>
+              <p>40,000원</p>
+            </div>
+          </div>
+          <button className="btn">주문하기</button>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
