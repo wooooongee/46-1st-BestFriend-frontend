ea952c39f79efeda99bb0f47220efecc83c7d14a
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-01 04:25:28 +0900

    Add : Main UI 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,50 +4,131 @@ import './Main.scss';
 const Main = () => {
   return (
     <main className="main">
-      <figure className="plant-video">
-        <img
-          src="https://github.com/pmpaca/test/blob/main/plant-lands.jpg?raw=true"
-          alt="plant-video"
+      <video loop autoPlay muted>
+        <source
+          className="main-video"
+          src="./videos/main-video.mp4"
+          type="video/mp4"
         />
-      </figure>
-      <section className="plants-container">
-        <div className="plants-message">
-          <div className="message title">
-            <h1>식물사세요</h1>
+      </video>
+      <section className="message-container">
+        <div className="message">
+          <h1 className="title">식물사세요.</h1>
+          <div className="description">
+            식물 기르는 것은 집안의 미세 먼지와 실내 공기를 깨끗하게 정화
+            시켜주는데 좋아요. 식물은 산소, 음이온 및 향기를 배출해서 집안의
+            공기를 깨끗하게 해주고 습도를 조절하여 기분을 좋게 해줘요.
           </div>
-          <div className="message description">
-            <p>설명설명설명</p>
-          </div>
+          <div className="message-cta">식물 살펴보기</div>
         </div>
-        <div className="plants-cards">
-          <div className="plant-card">plant card</div>
-          <div className="plant-card">plant card</div>
-          <div className="plant-card">plant card</div>
+        <div className="carousel">
+          <div className="carousel-btn">
+            <div className="left-btn">
+              <img className="arrow-left" src="./images/Main/arrow-left.png" />
+            </div>
+            <div className="right-btn">
+              <img
+                className="arrow-right"
+                src="./images/Main/arrow-right.png"
+              />
+            </div>
+          </div>
+          <div className="plant-cards">
+            <div className="plant-card">
+              <img
+                className="plant-image"
+                src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+                alt="plant-image"
+              />
+              <div className="card-cta-primary">더 알아보기</div>
+            </div>
+            <div className="plant-card">
+              <img
+                className="plant-image"
+                src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+                alt="plant-image"
+              />
+              <div className="card-cta-primary">더 알아보기</div>
+            </div>
+            <div className="plant-card">
+              <img
+                className="plant-image"
+                src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+                alt="plant-image"
+              />
+              <div className="card-cta-primary">더 알아보기</div>
+            </div>
+            <div className="plant-card">
+              <img
+                className="plant-image"
+                src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+                alt="plant-image"
+              />
+              <div className="card-cta-primary">더 알아보기</div>
+            </div>
+            <div className="plant-card">
+              <img
+                className="plant-image"
+                src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+                alt="plant-image"
+              />
+              <div className="card-cta-primary">더 알아보기</div>
+            </div>
+          </div>
         </div>
       </section>
       <section className="interior-container">
-        <div className="interior-message">
-          <h1>플랜테리어</h1>
-          <p>설명설명설명</p>
+        <div className="message">
+          <h1 className="title">플랜테리어.</h1>
+          <p className="description">
+            식물 기르는 것은 집안의 미세 먼지와 실내 공기를 깨끗하게 정화
+            시켜주는데 좋아요. 식물은 산소, 음이온 및 향기를 배출해서 집안의
+            공기를 깨끗하게 해주고 습도를 조절하여 기분을 좋게 해줘요.
+          </p>
         </div>
         <img
+          className="main-interior-image"
           src="https://github.com/pmpaca/test/blob/main/plant-lands.jpg?raw=true"
           alt="plant-interior"
         />
       </section>
       <section className="pots-container">
-        <div className="pots-message">
-          <h1>화분사세요</h1>
-          <p>설명설명설명</p>
+        <div className="message">
+          <h1 className="title">화분사세요.</h1>
+          <p className="description">
+            식물 기르는 것은 집안의 미세 먼지와 실내 공기를 깨끗하게 정화
+            시켜주는데 좋아요. 식물은 산소, 음이온 및 향기를 배출해서 집안의
+            공기를 깨끗하게 해주고 습도를 조절하여 기분을 좋게 해줘요.
+          </p>
         </div>
-        <div className="pots-cards">
-          <div className="pot-card">pot card</div>
-          <div className="pot-card">pot card</div>
-          <div className="pot-card">pot card</div>
+        <div className="pot-cards">
+          <div className="pot-card">
+            <img
+              className="pot-image"
+              src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+              alt="plant-image"
+            />
+            <div className="card-cta-secondary">더 알아보기</div>
+          </div>
+          <div className="pot-card">
+            <img
+              className="pot-image"
+              src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+              alt="plant-image"
+            />
+            <div className="card-cta-secondary">더 알아보기</div>
+          </div>
+          <div className="pot-card">
+            <img
+              className="pot-image"
+              src="https://github.com/pmpaca/test/blob/main/plant-15.jpg?raw=true"
+              alt="plant-image"
+            />
+            <div className="card-cta-secondary">더 알아보기</div>
+          </div>
         </div>
       </section>
     </main>
   );
 };
-
 export default Main;
