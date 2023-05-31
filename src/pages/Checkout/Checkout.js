a9fc305f92df384430b40442fbfc59b17c1895d1
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-31 22:32:00 +0900

    Modify: Checkout UI 수정사항반영

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CheckoutList from './CheckoutList';
 import './Checkout.scss';
 
 const Checkout = () => {
+  const [mockData, setMockData] = useState({});
+  const [mockDataList, setMockDataList] = useState([]);
+  const remainingPoint = parseInt(mockData.remainingPoint);
+  const totalprices = mockDataList.reduce(
+    (acc, cur) => acc + cur.prices * cur.count,
+    0
+  );
+
+  useEffect(() => {
+    fetch('/data/checkout.json')
+      .then(res => res.json())
+      .then(data => setMockData(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('/data/checkoutList.json')
+      .then(res => res.json())
+      .then(data => setMockDataList(data));
+  }, []);
+
   return (
     <main className="checkout">
       <article className="article-checkout">
@@ -9,64 +30,42 @@ const Checkout = () => {
         <section className="section-delivery-infomation">
           <h2 className="user-information-h2">배송 정보</h2>
           <div className="user-information-values">
-            <span className="user-name-value">홍길동</span>
-            <span className="user-address-value">
-              서울특별시 강남구 대치동 위워크 선릉 2호점 10층
+            <span className="user-name-value">{mockData.name}</span>
+            <span className="user-address-value">{mockData.address}</span>
+            <span className="user-phonenumber-value">
+              {mockData.phoneNumber}
             </span>
-            <span className="user-phonenumber-value">010-0000-0000</span>
           </div>
         </section>
         <section className="section-payment-point">
           <h2 className="remaining-point-h2">포인트로 결제하기</h2>
           <div className="remaining-point-value">
             잔여 포인트 :
-            <span className="remaining-point-span">500,000,000원</span>
+            <span className="remaining-point-span">
+              {remainingPoint.toLocaleString()} 원
+            </span>
           </div>
         </section>
         <section className="section-order-list">
           <h2 className="order-list-h2">주문 내역</h2>
           <ul className="order-list-ul">
-            <li className="order-list-li">
-              <img
-                className="order-list-img"
-                src="/images/Login/gron-logo.png"
-                alt="상품리스트이미지"
-              />
-              <div className="order-list-spans">
-                <span className="order-list-name">몬스테라</span>
-                <span className="order-list-count">수량 : 2개</span>
-                <span className="order-list-totalprices">
-                  총 가격 : 20,000원
-                </span>
-              </div>
-            </li>
-            <li className="order-list-li">
-              <img
-                className="order-list-img"
-                src="/images/Login/gron-logo.png"
-                alt="상품리스트이미지"
-              />
-              <div className="order-list-spans">
-                <span className="order-list-name">몬스테라</span>
-                <span className="order-list-count">수량 : 2개</span>
-                <span className="order-list-totalprices">
-                  총 가격 : 20,000원
-                </span>
-              </div>
-            </li>
+            <CheckoutList mockDataList={mockDataList} />
           </ul>
         </section>
       </article>
       <aside className="aside-checkout">
         <h2 className="checkout-h2">결제 금액</h2>
         <div className="product-prices">
-          상품 금액 <span>40,000원</span>
+          상품 금액 <span>{totalprices.toLocaleString()} 원</span>
         </div>
         <div className="delivery-fees">
           배송비 <span className="delivery-fees-value">무료</span>
         </div>
         <div className="total-prices">
-          총 결제 금액<span className="total-prices-value">40,000원</span>
+          총 결제 금액
+          <span className="total-prices-value">
+            {totalprices.toLocaleString()} 원
+          </span>
         </div>
         <button className="checkout-btn">결제 하기</button>
       </aside>
