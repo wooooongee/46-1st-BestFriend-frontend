a273a44509f1f49e16867d9e42bd57f26f7848d9
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-31 14:22:49 +0900

    Modify : style 수정 , CartBox 컴포넌트화

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -7,44 +7,8 @@ const Cart = () => {
       <h1 className="title">장바구니</h1>
       <div className="cart-container">
         <main className="cart-main">
-          <div className="cart-box">
-            <div className="cart-img">
-              <img
-                src="/images/ProductDetail/plant-21.jpg"
-                alt="product-img"
-                className="img"
-              />
-            </div>
-            <div className="cart-content">
-              <div className="content">
-                <h1 className="title">몬스테라</h1>
-                <p>수량 : 2개</p>
-                <button className="btn">삭제</button>
-              </div>
-              <div className="total">
-                <p>총 가격:20,000원</p>
-              </div>
-            </div>
-          </div>
-          <div className="cart-box">
-            <div className="cart-img">
-              <img
-                src="/images/ProductDetail/plant-21.jpg"
-                alt="product-img"
-                className="img"
-              />
-            </div>
-            <div className="cart-content">
-              <div className="content">
-                <h1 className="title">몬스테라</h1>
-                <p>수량 : 2개</p>
-                <button className="btn">삭제</button>
-              </div>
-              <div className="total">
-                <p>총 가격:20,000원</p>
-              </div>
-            </div>
-          </div>
+          <CartBox />
+          <CartBox />
         </main>
         <aside className="cart-aside">
           <h1 className="title">주문금액</h1>
@@ -72,3 +36,27 @@ const Cart = () => {
 };
 
 export default Cart;
+
+const CartBox = () => {
+  return (
+    <section className="cart-box">
+      <div className="cart-img">
+        <img
+          src="/images/ProductDetail/plant-21.jpg"
+          alt="product-img"
+          className="img"
+        />
+      </div>
+      <div className="cart-content">
+        <div className="content">
+          <h1 className="title">몬스테라</h1>
+          <p>수량 : 2개</p>
+          <button className="btn">삭제</button>
+        </div>
+        <div className="total">
+          <p>총 가격:20,000원</p>
+        </div>
+      </div>
+    </section>
+  );
+};
