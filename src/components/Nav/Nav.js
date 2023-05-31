6051c35c0c1b2232e2ffa6bcce010d62a6429a0a
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-31 10:33:56 +0900

    Add : 드롭다운 메뉴 UI 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,47 +1,82 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { PLANTS_SUBCATEGORY } from '../PlantsSubcategory/PlantsSubcategory';
+import { POTS_SUBCATEGORY } from '../PotsSubcategory/PotsSubcategory';
+import { TOOLS_SUBCATEGORY } from '../ToolsSubcategory/ToolsSubcategory';
 import './Nav.scss';
 const Nav = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="nav">
-      <img
-        className="logo"
-        src="./images/Nav/gron-logo.png"
-        alt="gron-logo"
-        onClick={() => navigate('/')}
-      />
-      <ul className="menus">
-        <li onClick={() => navigate('/plants')}>식물</li>
-        <li onClick={() => navigate('/pots')}>화분</li>
-        <li onClick={() => navigate('/tools')}>관리상품</li>
-      </ul>
-      <div className="search">
+    <>
+      <div className="nav">
         <img
-          className="search-icon"
-          src="./images/Nav/search.png"
-          alt="search-icon"
+          className="logo"
+          src="./images/Nav/gron-logo.png"
+          alt="gron-logo"
+          onClick={() => navigate('/')}
         />
-        <input />
+        <ul className="menus">
+          <li onClick={() => navigate('/plants')}>식물</li>
+          <li onClick={() => navigate('/pots')}>화분</li>
+          <li onClick={() => navigate('/tools')}>관리상품</li>
+        </ul>
+        <div className="search">
+          <img
+            className="search-icon"
+            src="./images/Nav/search.png"
+            alt="search-icon"
+          />
+          <input />
+        </div>
+        <div className="icons">
+          <img className="icon" src="./images/Nav/like.png" alt="like-icon" />
+          <img
+            className="icon"
+            src="./images/Nav/cart.png"
+            alt="cart-icon"
+            onClick={() => navigate('/cart')}
+          />
+          <button
+            className="login-btn"
+            type="button"
+            onClick={() => navigate('/login')}
+          >
+            로그인
+          </button>
+        </div>
       </div>
-      <div className="icons">
-        <img className="icon" src="./images/Nav/like.png" alt="like-icon" />
-        <img
-          className="icon"
-          src="./images/Nav/cart.png"
-          alt="cart-icon"
-          onClick={() => navigate('/cart')}
-        />
-        <button
-          className="login-btn"
-          type="button"
-          onClick={() => navigate('/login')}
-        >
-          로그인
-        </button>
+
+      <div className="dropdown-menu">
+        <ul className="plants-sub">
+          {PLANTS_SUBCATEGORY.map(plantsSub => {
+            return (
+              <li key={plantsSub.id} onClick={() => navigate('/plants')}>
+                {plantsSub.subcategoryName}
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="pots-sub">
+          {POTS_SUBCATEGORY.map(potsSub => {
+            return (
+              <li key={potsSub.id} onClick={() => navigate('/pots')}>
+                {potsSub.subcategoryName}
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="tools-sub">
+          {TOOLS_SUBCATEGORY.map(toolsSub => {
+            return (
+              <li key={toolsSub.id} onClick={() => navigate('/tools')}>
+                {toolsSub.subcategoryName}
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/ToolsSubcategory/ToolsSubcategory.js
@@ -0,0 +1,6 @@
+export const TOOLS_SUBCATEGORY = [
+  { id: 1, subcategoryName: '전체보기' },
+  { id: 2, subcategoryName: '스프레이' },
+  { id: 3, subcategoryName: '물뿌리개' },
+  { id: 4, subcategoryName: '모종삽' },
+];
