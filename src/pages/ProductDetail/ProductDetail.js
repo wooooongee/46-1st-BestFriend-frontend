f29418d6616528dacaeaadd87abb282a2b6e3aec
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-31 14:13:38 +0900

    Modify : Card 컴포넌트화 , 스타일 수정

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -2,56 +2,70 @@ import React, { useState } from 'react';
 import './ProductDetail.scss';
 
 const ProductDetail = () => {
-  let [count, setCount] = useState(1);
+  const [count, setCount] = useState(1);
+  const [hover, setHover] = useState(false);
   return (
     <>
-      <div className="product-detail">
-        <div className="product-img">
-          <img src="/images/ProductDetail/plant-21.jpg" alt="product-img" />
-        </div>
-        <div className="product-content">
+      <main className="product-detail">
+        <figure className="product-img">
+          <img
+            src="/images/ProductDetail/plant-21.jpg"
+            alt="product-img"
+            className="img"
+          />
+        </figure>
+        <section className="product-content">
           <p className="p-tag">#초보자도 키우기 쉬워요</p>
-          <h3 className="title">몬스테라</h3>
+          <h1 className="title">몬스테라</h1>
           <p className="p-tag">10,000원</p>
           <p className="p-tag">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto
-            magnam aut sunt nulla nobis nesciunt eos voluptas qui ipsam ipsum
-            distinctio
+            magnam aut sunt nulla nobis nesciunt eos voluptas
           </p>
           <div className="count-container">
             <button className="btn">
-              <img src="/images/ProductDetail/arrow-down.png" alt="arrow-img" />
+              <img
+                src="/images/ProductDetail/arrow-down.png"
+                alt="arrow-img"
+                className="img"
+              />
             </button>
             <div className="count-box">{count}</div>
             <button className="btn">
-              <img src="/images/ProductDetail/arrow-up.png" alt="" />
+              <img
+                src="/images/ProductDetail/arrow-up.png"
+                alt="arrow-img"
+                className="img"
+              />
             </button>
           </div>
           <button className="btn cart-btn">장바구니 담기</button>
-        </div>
-      </div>
-      <div className="product-recommend">
+        </section>
+      </main>
+      <section className="product-recommend">
         <p>이런 식물은 어때요?</p>
         <div className="recommend-container">
-          <div className="recommend">
-            <img src="/images/ProductDetail/plant-21.jpg" alt="" />
-            <p className="title">몬스테라</p>
-            <p className="p-tag">10,000원</p>
-          </div>
-          <div className="recommend">
-            <img src="/images/ProductDetail/plant-21.jpg" alt="" />
-            <p className="title">몬스테라</p>
-            <p className="p-tag">10,000원</p>
-          </div>
-          <div className="recommend">
-            <img src="/images/ProductDetail/plant-21.jpg" alt="" />
-            <p className="title">몬스테라</p>
-            <p className="p-tag">10,000원</p>
-          </div>
+          <Recommend />
+          <Recommend />
+          <Recommend />
         </div>
-      </div>
+      </section>
     </>
   );
 };
 
 export default ProductDetail;
+
+const Recommend = () => {
+  return (
+    <div className="recommend">
+      <img
+        src="/images/ProductDetail/plant-21.jpg"
+        alt="product-img"
+        className="img"
+      />
+      <p className="title">몬스테라</p>
+      <p>10,000원</p>
+    </div>
+  );
+};
