602c9bd2aa66358ddd31cef87fcf8ee0c9d61f7c
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-01 18:00:29 +0900

    Modify: 수정사항반영

--- src/pages/Checkout/CheckoutList.js
+++ src/pages/Checkout/CheckoutList.js
@@ -2,23 +2,26 @@ import React from 'react';
 import './CheckoutList.scss';
 
 const CheckoutList = ({ mockDataList }) => {
-  const showMockDataList = mockDataList.map(list => (
-    <li className="order-list-li" key={list.id}>
-      <img
-        className="order-list-img"
-        src="/images/Login/gron-logo.png"
-        alt="상품리스트이미지"
-      />
-      <div className="order-list-spans">
-        <span className="order-list-name">{list.name}</span>
-        <span className="order-list-count">수량 : {list.count}개</span>
-        <span className="order-list-totalprices">
-          총 가격 : {(list.prices * list.count).toLocaleString()} 원
-        </span>
-      </div>
-    </li>
-  ));
-  return showMockDataList;
+  return (
+    <>
+      {mockDataList.map(list => (
+        <li className="order-list-li" key={list.id}>
+          <img
+            className="order-list-img"
+            src="/images/Login/gron-logo.png"
+            alt="상품리스트이미지"
+          />
+          <div className="order-list-spans">
+            <span className="order-list-name">{list.name}</span>
+            <span className="order-list-count">수량 : {list.count}개</span>
+            <span className="order-list-totalprices">
+              총 가격 : {(list.prices * list.count).toLocaleString()} 원
+            </span>
+          </div>
+        </li>
+      ))}
+    </>
+  );
 };
 
 export default CheckoutList;
