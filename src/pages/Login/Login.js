17201d061f4cb12133a89d27f597c3dd842b72eb
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-01 17:09:05 +0900

    Feat: 로그인 UI 수정 및 로그인 인증 기능 구현
    - 로그인 버튼 스타일을 수정했습니다.
    - 이메일input에 @ 포함, 비밀번호input의 길이가 5이상 유효성 검사,실패시 alert창을 띄웁니다.
    - 로그인 인증이 성공하면 main으로 이동 및 token을 local에 저장, 실패하면 alert창 띄웁니다.
    - 계정만들기 버튼 클릭시 signup 페이지로 이동

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  const [userInputs, setUserInputs] = useState({
+    userId: '',
+    userPassword: '',
+  });
+
+  const navigate = useNavigate();
+
+  const { userId, userPassword } = userInputs;
+
+  const isValid = userId.includes('@') && userPassword.length >= 5;
+
+  const handleLoginBtn = () => {
+    if (!isValid) {
+      alert('이메일 혹은 비밀번호를 확인해주세요');
+
+      return;
+    }
+
+    fetch('http://10.58.52.136:3000/users/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: userId,
+        password: userPassword,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.accessToken) {
+          localStorage.setItem('token', data.accessToken);
+          navigate('/main');
+        } else if (data.message === 'INVALID_USER') {
+          alert('이메일 혹은 비밀번호를 확인 해 주세요');
+        }
+      });
+  };
+
+  const handleUserInputs = e => {
+    const { name, value } = e.target;
+    setUserInputs({
+      ...userInputs,
+      [name]: value,
+    });
+  };
+
   return (
     <main className="login-container">
       <img
@@ -9,10 +57,35 @@ const Login = () => {
         src="/images/Login/gron-logo.png"
         alt="gronLogo"
       />
-      <input className="id-input" placeholder="이메일" />
-      <input className="password-input" placeholder="비밀번호" />
-      <button className="login-btn">로그인</button>
-      <button className="signup-btn">계정 만들기</button>
+      <input
+        type="email"
+        className="id-input"
+        name="userId"
+        placeholder="이메일"
+        required
+        value={userId}
+        onChange={handleUserInputs}
+      />
+
+      <input
+        type="password"
+        className="password-input"
+        name="userPassword"
+        placeholder="비밀번호"
+        value={userPassword}
+        onChange={handleUserInputs}
+      />
+      <button className="login-btn" onClick={handleLoginBtn}>
+        로그인
+      </button>
+      <button
+        className="signup-btn"
+        onClick={() => {
+          navigate('/signup');
+        }}
+      >
+        계정 만들기
+      </button>
     </main>
   );
 };
