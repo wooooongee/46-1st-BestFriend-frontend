f1ac9ca2b99975de80cedcabc0a2314cdb33d28a
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-06-02 19:01:59 +0900

    Modify : style 수정 , 이미지 경로 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -17,7 +17,7 @@ const Nav = () => {
       <nav className="navbar">
         <img
           className="logo"
-          src={process.env.PUBLIC_URL + '/images/Nav/gron-logo.png'}
+          src="/images/Nav/gron-logo.png"
           alt="gron-logo"
           onClick={() => navigate('/')}
         />
@@ -40,20 +40,16 @@ const Nav = () => {
         <div className="search">
           <img
             className="search-icon"
-            src={process.env.PUBLIC_URL + '/images/Nav/search.png'}
+            src="/images/Nav/search.png"
             alt="search-icon"
           />
           <input className="search-input" />
         </div>
         <div className="icons">
+          <img className="icon" src="/images/Nav/like.png" alt="like-icon" />
           <img
             className="icon"
-            src={process.env.PUBLIC_URL + '/images/Nav/like.png'}
-            alt="like-icon"
-          />
-          <img
-            className="icon"
-            src={process.env.PUBLIC_URL + '/images/Nav/cart.png'}
+            src="/images/Nav/cart.png"
             alt="cart-icon"
             onClick={() => navigate('/cart')}
           />
