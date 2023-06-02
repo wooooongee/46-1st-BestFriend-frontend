70f5be5d71d1f3357bf1a9d66ffaeec8d895edc9
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-03 00:24:39 +0900

    WIP

--- src/components/MainMessage/MainMessage.js
+++ src/components/MainMessage/MainMessage.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './MainMessage.scss';
 
-const MainMessage = () => {
+const MainMessage = ({ title, description }) => {
   return (
     <div className="main-message">
-      <h1 className="title">편리하게 식물을 입양하세요.</h1>
-      <div className="description">
-        여려움 없이 식물을 입양할 수 있어요. 초보 집사도, 다양한 식물을 구하는
-        집사도 모두 환영합니다.
-      </div>
+      <h1 className="title">{title}</h1>
+      <div className="description">{description}</div>
     </div>
   );
 };
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -10,13 +10,21 @@ const Main = () => {
       <video loop autoPlay muted>
         <source
           className="main-video"
-          src="https://vod-progressive.akamaized.net/exp=1685618559~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2674%2F20%2F513373978%2F2375646382.mp4~hmac=eeec08dc177f158ced4a3d7a60f20d9b0a5cd713041a716e53543882f8649f65/vimeo-prod-skyfire-std-us/01/2674/20/513373978/2375646382.mp4"
+          src="https://ik.imagekit.io/zawntqhuq/main-video.mp4?updatedAt=1685715072261"
           type="video/mp4"
         />
       </video>
       <section className="plant-container">
-        <MainMessage />
-        <div className="message-cta">식물 살펴보기</div>
+        <MainMessage
+          title={MESSAGE[0].title}
+          description={MESSAGE[0].description}
+        />
+        <div
+          className="message-cta"
+          onClick={() => navigate(`list?category=1,2,3,4`)}
+        >
+          식물 살펴보기
+        </div>
         <div className="carousel">
           <div className="carousel-btn">
             <div className="left-btn">
@@ -30,30 +38,78 @@ const Main = () => {
             </div>
           </div>
           <div className="plant-cards">
-            <MainPlantCard />
-            <MainPlantCard />
-            <MainPlantCard />
-            <MainPlantCard />
+            <MainPlantCard
+              id={36}
+              image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-36.jpg?raw=true"
+            />
+            <MainPlantCard
+              id={4}
+              image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-4.jpg?raw=true"
+            />
+            <MainPlantCard
+              id={23}
+              image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-23.jpg?raw=true"
+            />
+            <MainPlantCard
+              id={11}
+              image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-11.jpg?raw=true"
+            />
           </div>
         </div>
       </section>
       <section className="interior-container">
-        <MainMessage />
+        <MainMessage
+          title={MESSAGE[1].title}
+          description={MESSAGE[1].description}
+        />
         <img
           className="main-interior-image"
-          src="https://github.com/pmpaca/test/blob/main/plant-lands.jpg?raw=true"
+          src="https://ik.imagekit.io/zawntqhuq/interior-01.png?updatedAt=1685718153515"
           alt="plant-interior"
         />
       </section>
       <section className="pots-container">
-        <MainMessage />
+        <MainMessage
+          title={MESSAGE[2].title}
+          description={MESSAGE[2].description}
+        />
         <div className="pot-cards">
-          <MainPotCard />
-          <MainPotCard />
-          <MainPotCard />
+          <MainPotCard
+            id={62}
+            image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-62.jpg?raw=true"
+          />
+          <MainPotCard
+            id={65}
+            image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-65.jpg?raw=true"
+          />
+          <MainPotCard
+            id={68}
+            image_url="https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-68.jpg?raw=true"
+          />
         </div>
       </section>
     </main>
   );
 };
 export default Main;
+
+const MESSAGE = [
+  {
+    id: 1,
+    title: '편리하게 식물을 입양하세요.',
+    description:
+      '어려움 없이 식물을 입양할 수 있어요. 초보 집사도, 다양한 식물을 구하는 집사도 모두 환영합니다. 어떤 식물이 마음에 드는지 천천히 살펴보세요.',
+  },
+  {
+    id: 2,
+    title: '인테리어도 식물과 함께하세요.',
+    description:
+      '식물은 인테리어에 생기를 불어넣어주는 데 효과적이에요. 집을 더 아름답고, 건강하고, 가치있게 만들어 주죠. 플랜테리어 사진 갤러리에서 식물을 활용한 인테리어 아이디어를 얻어보세요.',
+  },
+  {
+    id: 3,
+    title: '식물도 예쁜 집을 좋아해요.',
+    description:
+      '화분은 식물을 키우는 데 가장 중요한 요소 중 하나입니다. 적절한 크기의 화분은 식물이 잘 자랄 수 있게 도와주죠. 또 화분은 식물과 함께 인테리어에 화려함과 조화를 더해줘요.',
+  },
+];
