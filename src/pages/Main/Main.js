cb6dd8b7161a8df5167235ef1424909f3e19d696
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-03 13:29:48 +0900

    Refactor

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import MainMessage from '../../components/MainMessage/MainMessage';
 import MainPlantCard from '../../components/MainPlantCard/MainPlantCard';
 import MainPotCard from '../../components/MainPotCard/MainPotCard';
