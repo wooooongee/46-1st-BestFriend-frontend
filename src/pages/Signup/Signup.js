9a4cfe75e1eaf5c1135f502108d08eb0dfca9d8c
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-03 12:23:44 +0900

    Feat: INPUT 입력값받기

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import './Signup.scss';
 
 const Signup = () => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone: '',
+    address: '',
+  });
+
+  const [errorMessages, setErrorMessages] = useState({
+    nameMessage: '',
+    emailMessage: '',
+    passwordMessage: '',
+    confirmPasswordMessage: '',
+    phoneMessage: '',
+    addressMessage: '',
+  });
+
+  const { name, email, password, confirmPassword, phone, address } = inputs;
+
+  const {
+    nameMessage,
+    emailMessage,
+    passwordMessage,
+    confirmPasswordMessage,
+    phoneMessage,
+    addressMessage,
+  } = errorMessages;
+
+  const navigate = useNavigate();
+
+  // const onClick = () => {
+  //   fetch('http://10.58.52.112:3000/users/signup', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       name: '',
+  //       email: '',
+  //       password: '',
+  //       phone: '',
+  //       address: '',
+  //     }),
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       if (data.message === 'user is created') {
+  //         alert('성공');
+  //         navigate('/login');
+  //       }
+  //     });
+  // };
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs(prev => ({ ...prev, [name]: value }));
+  };
+
   return (
     <main className="signup">
       <img
@@ -9,12 +69,14 @@ const Signup = () => {
         src="/images/Login/gron-logo.png"
         alt="gronLogo"
       />
-      {USER_INFO_INPUTS.map(info => (
+      {USER_INFO_INPUTS.map(input => (
         <input
           className="user-input"
-          key={info.id}
-          name={info.id}
-          placeholder={info.placeholder}
+          key={input.id}
+          name={input.id}
+          placeholder={input.placeholder}
+          onChange={onChange}
+          value={inputs[input.id]}
         />
       ))}
       <div className="privacy-agreement-input">
@@ -29,9 +91,10 @@ const Signup = () => {
 export default Signup;
 
 const USER_INFO_INPUTS = [
-  { id: 1, placeholder: '이름' },
-  { id: 2, placeholder: '이메일' },
-  { id: 3, placeholder: '비밀번호' },
-  { id: 4, placeholder: '휴대폰 번호' },
-  { id: 5, placeholder: '주소' },
+  { id: 'name', placeholder: '이름' },
+  { id: 'email', placeholder: '이메일' },
+  { id: 'password', placeholder: '비밀번호' },
+  { id: 'confirmPassword', placeholder: '비밀번호 확인' },
+  { id: 'phone', placeholder: '휴대폰 번호' },
+  { id: 'address', placeholder: '주소' },
 ];
