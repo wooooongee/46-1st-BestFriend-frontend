99d17c16f356f2dec908990115fa820c1fe62930
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-04 18:24:46 +0900

    Feat: login,signup에서 Nav,Footer 분리

--- src/Router.js
+++ src/Router.js
@@ -2,28 +2,27 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
 import Cart from './pages/Cart/Cart';
 import Checkout from './pages/Checkout/Checkout';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductList from './pages/ProductList/ProductList';
 import Signup from './pages/Signup/Signup';
+import Container from './Container';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route element={<Container />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product" element={<ProductDetail />} />
+          <Route path="/productlist" element={<ProductList />} />
+        </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/product" element={<ProductDetail />} />
-        <Route path="/productlist" element={<ProductList />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
