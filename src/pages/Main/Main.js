7e45295836f47bbc5bf7f7217dc360b5e0520496
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-05 13:53:43 +0900

    Modify : 상수데이터 별도 파일로 분리

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import MainMessage from '../../components/MainMessage/MainMessage';
 import MainPlantCard from '../../components/MainPlantCard/MainPlantCard';
 import MainPotCard from '../../components/MainPotCard/MainPotCard';
+import {
+  MESSAGE,
+  PLANT_CARD,
+  POT_CARD,
+} from '../../components/MainContent/MainContent';
 import './Main.scss';
 
 const Main = () => {
@@ -93,80 +98,3 @@ const Main = () => {
   );
 };
 export default Main;
-
-const MESSAGE = [
-  {
-    id: 1,
-    title: '편리하게 식물을 입양하세요.',
-    description:
-      '어려움 없이 식물을 입양할 수 있어요. 초보 집사도, 다양한 식물을 구하는 집사도 모두 환영합니다. 어떤 식물이 마음에 드는지 천천히 살펴보세요.',
-  },
-  {
-    id: 2,
-    title: '인테리어도 식물과 함께하세요.',
-    description:
-      '식물은 인테리어에 생기를 불어넣어주는 데 효과적이에요. 집을 더 아름답고, 건강하고, 가치있게 만들어 주죠. 플랜테리어 사진 갤러리에서 식물을 활용한 인테리어 아이디어를 얻어보세요.',
-  },
-  {
-    id: 3,
-    title: '식물도 예쁜 집을 좋아해요.',
-    description:
-      '화분은 식물을 키우는 데 가장 중요한 요소 중 하나입니다. 적절한 크기의 화분은 식물이 잘 자랄 수 있게 도와주죠. 또 화분은 식물과 함께 인테리어에 화려함과 조화를 더해줘요.',
-  },
-];
-
-const PLANT_CARD = [
-  {
-    id: 36,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-36.jpg?raw=true',
-  },
-  {
-    id: 4,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-4.jpg?raw=true',
-  },
-  {
-    id: 23,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-23.jpg?raw=true',
-  },
-  {
-    id: 11,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-11.jpg?raw=true',
-  },
-  {
-    id: 15,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-15.jpg?raw=true',
-  },
-  {
-    id: 20,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-20.jpg?raw=true',
-  },
-  {
-    id: 25,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%EC%8B%9D%EB%AC%BC%20(60)/plant-25.jpg?raw=true',
-  },
-];
-
-const POT_CARD = [
-  {
-    id: 62,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-62.jpg?raw=true',
-  },
-  {
-    id: 65,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-65.jpg?raw=true',
-  },
-  {
-    id: 68,
-    image_url:
-      'https://github.com/HaeJungg/project-image/blob/master/Image%20-%20%ED%99%94%EB%B6%84(10)/pot-68.jpg?raw=true',
-  },
-];
