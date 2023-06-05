5fea804a09072ba84ac9cda58267643c922f824c
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-05 19:47:43 +0900

    Refactor

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CATEGORIES } from '../Category/Category';
 import './Dropdown.scss';
+
+const DROPDOWN_MENU = Object.values(CATEGORIES);
+
 const Dropdown = ({ setIsMenuOpen }) => {
   const navigate = useNavigate();
   return (
     <div className="dropdown" onMouseLeave={() => setIsMenuOpen(false)}>
-      {NAV_MENU.map(({ title, path, subCategories }) => {
+      {DROPDOWN_MENU.map(({ title, path, subCategories }) => {
         return (
           <ul key={title} className="dropdown-sub">
             <li className="main-category-all" onClick={() => navigate(path)}>
@@ -31,5 +34,3 @@ const Dropdown = ({ setIsMenuOpen }) => {
 };
 
 export default Dropdown;
-
-const NAV_MENU = Object.values(CATEGORIES);
