4e35ced1a3dc9a0025b30c2e5cda2da8f94640e1
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-07 20:45:20 +0900

    WIP

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -14,51 +14,60 @@ const Signup = () => {
   const [check, setCheck] = useState(false);
   const navigate = useNavigate();
   const { name, email, password, confirmpassword, phone, address } = inputs;
+
   const userInfoValid = {
-    name:
-      !/^[가-힣]{2,4}$/.test(name) &&
-      name.length > 0 &&
-      '한글 2~4글자로 입력하세요',
-    email:
-      !/^[a-z0-9\-_]+@([a-z0-9]+\.com)$/.test(email) &&
-      email.length > 0 &&
-      '잘못된 이메일 주소입니다',
-    password:
-      !/^(?=.*[A-Z]).{8,}$/.test(password) &&
-      password.length > 0 &&
-      '영문 대문자를 포함하여 8자이상 입력하세요',
-    confirmpassword:
-      password !== confirmpassword &&
-      confirmpassword.length > 0 &&
-      '비밀번호가 일치하지 않습니다',
-    phone:
-      !/^[0-9]{3}[0-9]{4}[0-9]{4}$/.test(phone) &&
-      phone.length > 0 &&
-      '11자리 숫자만 입력하세요',
-    address:
-      !/^[가-힣0-9 ]+$/.test(address) &&
-      address.length > 0 &&
-      '한글,숫자만 입력하세요',
+    name: /^[가-힣]{2,4}$/.test(name),
+    email: /^[a-z0-9\-_]+@([a-z0-9]+\.com)$/.test(email),
+    password: /^(?=.*[A-Z]).{8,}$/.test(password),
+    confirmpassword: password === confirmpassword,
+    phone: /^[0-9]{3}[0-9]{4}[0-9]{4}$/.test(phone),
+    address: /^[가-힣0-9 ]+$/.test(address),
+  };
+
+  const allTrue = currentValue => currentValue === true;
+
+  const alertMsg = {
+    name: userInfoValid.name
+      ? '올바르게 입력했어요'
+      : '한글 2~4글자로 입력하세요',
+    email: userInfoValid.email
+      ? '올바르게 입력했어요'
+      : '잘못된 이메일 주소입니다',
+    password: userInfoValid.password
+      ? '올바르게 입력했어요'
+      : '영문 대문자를 포함하여 8자이상 입력하세요',
+    confirmpassword: userInfoValid.confirmpassword
+      ? '올바르게 입력했어요'
+      : '비밀번호가 일치하지 않습니다',
+    phone: userInfoValid.phone
+      ? '올바르게 입력했어요'
+      : '11자리 숫자만 입력하세요',
+    address: userInfoValid.address
+      ? '올바르게 입력했어요'
+      : '한글,숫자만 입력하세요',
   };
 
   const handleSignupBtn = () => {
-    fetch('http://10.58.52.117:3000/users/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(inputs),
-    })
-      .then(res => res.json())
-      .then(data => {
-        if (data.message === 'user is created') {
-          alert('성공');
-          navigate('/login');
-        } else {
-          alert('실패');
-        }
-      });
+    Object.values(userInfoValid).every(allTrue)
+      ? fetch('http://10.58.52.117:3000/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(inputs),
+        })
+          .then(res => res.json())
+          .then(data => {
+            if (data.message === 'user is created') {
+              alert('성공');
+              navigate('/login');
+            } else {
+              alert('실패');
+            }
+          })
+      : alert('올바르게 입력하지 않은 칸이 있어요');
   };
+
   const onChange = e => {
     const { name, value } = e.target;
     setInputs(prev => ({ ...prev, [name]: value }));
@@ -85,7 +94,13 @@ const Signup = () => {
                 : 'text'
             }
           />
-          <p className="error-messages-p">{userInfoValid[input.id]}</p>
+          <p
+            className={`error-messages-p ${
+              userInfoValid[input.id] ? 'green' : ''
+            }`}
+          >
+            {inputs[input.id].length > 0 && alertMsg[input.id]}
+          </p>
         </React.Fragment>
       ))}
       <div className="privacy-agreement-input">
