9b7b1357d2fc3242da1407ad0bdfb01c162c5723
Kris <123653529+pmpaca@users.noreply.github.com>
2023-06-07 17:39:16 +0900

    Modify : API 적용

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Review.scss';
+import { APIS } from '../../config';
 
-const Review = () => {
+const Review = ({ productsId, token }) => {
   const [isToggleOn, setIsToggleOn] = useState(false);
   const [reviewList, setReviewList] = useState([]);
   const [reviewText, setReviewText] = useState('');
@@ -10,11 +11,47 @@ const Review = () => {
     setReviewText(e.target.value);
   };
 
-  const handleKeyup = e => {
-    if (e.key === 'Enter' && reviewText !== '') {
-      setReviewList(reviewList => [...reviewList, reviewText]);
-      setReviewText('');
-    }
+  // // Review GET
+  useEffect(() => {
+    fetch(`${APIS.reviews}/${productsId}`)
+      .then(res => res.json())
+      .then(data => {
+        setReviewList(data.Review);
+      });
+  }, [productsId]);
+
+  // Review POST
+  const handleReviewPost = e => {
+    fetch(`${APIS.reviews}/${productsId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        product_id: productsId,
+        comment: reviewText,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        setReviewList(prev => [...prev, reviewText]);
+        setReviewText('');
+        window.location.reload();
+      });
+  };
+
+  // Review DELETE
+  const handleReviewDelete = () => {
+    fetch(`${APIS.reviews}/${reviewList.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: token,
+      },
+    })
+      .then(res => res.json())
+      .then(window.location.reload());
   };
 
   return (
@@ -34,15 +71,36 @@ const Review = () => {
 
       {isToggleOn && (
         <div className="reviews-container">
-          {reviewList.map((comment, index) => {
+          {reviewList.map(({ id, name, created_at, comment }) => {
+            const convertedTime = Date(created_at * 1000).toLocaleString(
+              'en-US',
+              {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false,
+              }
+            );
+
+            const [weekday, month, day, year, timePart, timezone] =
+              convertedTime.split(' ');
+            const [hour, minute] = timePart.split(':');
+
+            const formattedTime = `${month} ${day} ${year}, ${hour}:${minute}`;
+
             return (
-              <div className="review-posted" key={index}>
+              <div className="review-posted" key={id}>
                 <div className="review-top">
                   <div className="review-info">
-                    <div className="username">식물왕</div>
-                    <div className="timestamp">2023년 6월 9일 17:09</div>
+                    <div className="username">{name}</div>
+                    <div className="timestamp">{formattedTime}</div>
                   </div>
-                  <div className="trash">삭제</div>
+                  <div className="trash" onClick={handleReviewDelete}>
+                    삭제
+                  </div>
                 </div>
 
                 <div className="review-text">{comment}</div>
@@ -54,7 +112,9 @@ const Review = () => {
             type="text"
             placeholder="리뷰 내용을 작성한 후 엔터키를 누르세요."
             onChange={handleText}
-            onKeyUp={handleKeyup}
+            onKeyUp={e => {
+              e.key === 'Enter' && reviewText !== '' && handleReviewPost();
+            }}
             value={reviewText}
           />
         </div>
